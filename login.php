f63cdb97fd9b05d27060987634bea9ec8ebd0901
SiyaPockle <166788343+SiyaPockle@users.noreply.github.com>
2024-05-05 15:38:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -49,7 +49,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+<title>Livestream</title>
+<link rel="icon" href="images/blood.jpg">
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
