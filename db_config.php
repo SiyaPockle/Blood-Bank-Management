0eee8553835eeb103b75f1b8455d97fc347aff85
SiyaPockle <166788343+SiyaPockle@users.noreply.github.com>
2024-05-05 15:16:14 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "p2";
+$dbname = "db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
